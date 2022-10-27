852ff05a718b85f808cb29d393262db2644da2a7
pr1s10n3r <alvarostagg@protonmail.com>
2022-10-27 02:08:43 -0500

    nvim: moving specific configuration to external files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,42 +1,12 @@
-local g   = vim.g
-local opt = vim.opt
-
---------------------------
--- Core Editor Settings --
---------------------------
-opt.number      = true
-opt.mouse       = 'a'
-opt.ignorecase  = true
-opt.smartcase   = true
-opt.hlsearch    = false
-opt.wrap        = false
-opt.colorcolumn = '120'
-opt.splitright  = true
-opt.splitbelow  = true
-opt.showmatch   = true
-opt.foldmethod  = 'marker'
-
--- Tabs
-opt.tabstop     = 4
-opt.shiftwidth  = 4
-opt.expandtab   = true
-opt.smartindent = true
-
-g.mapleader = ' '
-
 -- Clipboard Hacking
 vim.keymap.set({'n', 'x'}, 'cp', '"y')
 vim.keymap.set({'n', 'x'}, 'cv', '"+p')
 vim.keymap.set({'n', 'x'}, 'x', '"_x')
 
--- Theme
-opt.termguicolors = true
-vim.cmd('colorscheme gruvbox')
-
------------------------
--- Plugin Management --
------------------------
 require('packer_init')
+require('core/options')
+require('core/keymaps')
+require('core/theme')
 require('lsp/lspconfig')
 require('plugins/nvim-cmp')
 require('plugins/nvim-treesitter')
---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,19 @@
+-- Define neovim and plugins keymaps
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Set leader key to comma
+vim.g.mapleader = ','
+
+-- Disable arrow keys
+map('', '<up>', '<nop>')
+map('', '<down>', '<nop>')
+map('', '<left>', '<nop>')
+map('', '<right>', '<nop>')
+
+
