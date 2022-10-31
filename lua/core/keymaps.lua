dc05cb1af4e3413ec8b220b5c897c86cfda5c5ef
pr1s10n3r <alvarostagg@protonmail.com>
2022-10-31 08:13:17 -0500

    nvim: add telescope plugin

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,22 @@
+-- Define neovim and plugins keymaps
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-- Set leader key to comma
+vim.g.mapleader = ','
+
+-- Disable arrow keys
+map('', '<up>', '<nop>')
+map('', '<down>', '<nop>')
+map('', '<left>', '<nop>')
+map('', '<right>', '<nop>')
+
+-- NvimTree
+map('n', '<C-n>', ':NvimTreeToggle<CR>')            -- open/close
+map('n', '<leader>f', ':NvimTreeRefresh<CR>')       -- refresh
+map('n', '<leader>n', ':NvimTreeFindFile<CR>')      -- search file
